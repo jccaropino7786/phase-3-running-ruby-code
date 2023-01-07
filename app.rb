daeb0bce5b360ca36ce8dd5f98a96133213d8cd6
john Caropino <jccaropino7786@live.com>
2023-01-07 12:29:52 -0800

    commit tree 1be364ea14d599d89d567e9a0029f424e30fa28b

--- app.rb
+++ app.rb
@@ -1,18 +1,14 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+puts "Hello World!"
 
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
 
-# def add
-    
-# end
-# # 1 + 1
-# puts add + add
+print "Pass this test, please."
+
+
+p [1, 2, 3]
+
+
+
+
 
 def add num1, num2
     puts num1
